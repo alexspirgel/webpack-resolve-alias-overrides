b22074dcca05557b13afa97f69f2a0709e44aa42
Alexander Spirgel <spirgel.alexander@gmail.com>
2019-07-10 00:43:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+class ResolveAliasOverrides {
+	constructor(aliasOverridesObject) {
+		this.aliasOverridesObject = aliasOverridesObject;
+	}
+	apply(resolver) {
+		const target = resolver.ensureHook('resolve');
+		resolver.getHook('described-resolve').tapAsync('ResolveAliasOverrides', (request, resolveContext, callback) => {
+			for (const alias in this.aliasOverridesObject) {
+				const thisAlias = this.aliasOverridesObject[alias];
+				if (request.request.indexOf(alias) > -1) {
+					for (const path in thisAlias) {
+						const thisPath = thisAlias[path];
+						const newRequestStr = request.request.replace(alias, thisPath);
+						console.log(newRequestStr);
+						if (fs.existsSync(newRequestStr)) {
+							console.log('^ FOUND');
+							const obj = Object.assign({}, request, {
+								request: newRequestStr
+							});
+							return resolver.doResolve(target, obj, "aliased with mapping '" + alias + "': '" + thisPath + "' to '" + newRequestStr + "'", resolveContext, (err, result) => {
+								if (err) {
+									return callback(err);
+								}
+								if (result === undefined) {
+									return callback(null, null);
+								}
+								callback(null, result);
+							});
+						}
+					}
+				}
+			}
+			return callback();
+		});
+	}
+}
+
+module.exports = ResolveAliasOverrides;
