e22be5872945d3df2807067e756afed0e8d04135
Alexander Spirgel <spirgel.alexander@gmail.com>
2019-07-12 18:11:53 -0400

    Changed variable names and added more comments

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 class ResolveAliasOverrides {
 	constructor(aliasOverridesObject) {
+		// Set the alias overrides object to the class instance.
 		this.aliasOverridesObject = aliasOverridesObject;
 	}
 	apply(resolver) {
 		const target = resolver.ensureHook('resolve');
 		resolver.getHook('described-resolve').tapAsync('ResolveAliasOverrides', (request, resolveContext, callback) => {
-			for (const alias in this.aliasOverridesObject) {
-				const thisAlias = this.aliasOverridesObject[alias];
-				if (request.request.indexOf(alias) > -1) {
-					for (const path in thisAlias) {
-						const thisPath = thisAlias[path];
-						const newRequestStr = request.request.replace(alias, thisPath);
-						if (fs.existsSync(newRequestStr)) {
+			// For each match string in the alias overrides object.
+			for (const matchString in this.aliasOverridesObject) {
+				// If the match string exists in the request path.
+				if (request.request.indexOf(matchString) > -1) {
+					// Get the aliases for the match string.
+					const aliases = this.aliasOverridesObject[matchString];
+					// For each alias.
+					for (const aliasIndex in aliases) {
+						// Get this alias.
+						const alias = aliases[aliasIndex];
+						// Replace the match string with this alias.
+						const newRequestString = request.request.replace(matchString, alias);
+						// If the file exists.
+						if (fs.existsSync(newRequestString)) {
 							const obj = Object.assign({}, request, {
-								request: newRequestStr
+								request: newRequestString
 							});
-							return resolver.doResolve(target, obj, "aliased with mapping '" + alias + "': '" + thisPath + "' to '" + newRequestStr + "'", resolveContext, (err, result) => {
-								if (err) {
-									return callback(err);
+							return resolver.doResolve(target, obj, "aliased with mapping '" + matchString + "': '" + alias + "' to '" + newRequestString + "'", resolveContext, (error, result) => {
+								if (error) {
+									return callback(error);
 								}
 								if (result === undefined) {
 									return callback(null, null);
